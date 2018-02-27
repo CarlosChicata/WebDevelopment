bcc1d86af7b62c1c34af12af8bdc5595221e4a11
Carlos Chicata Farfan <carlos.fernando.chicata@gmail.com>
2018-02-27 11:33:42 -0500

    change in the finding component in header components- frontend

--- RestaFind/static/js/Logistica.js
+++ RestaFind/static/js/Logistica.js
@@ -5,5 +5,26 @@
     pero escogimos esto por un motivos mas: aprendizaje sobre componentes en Vue.js
 **/
 
+var app1 = new Vue({
+    el:"#Opciones",
+    data:{
+        City:[
+            { text:"choose your place"},
+            {text:"camana, arequipa"},
+            {text:"ocoña, arequipa"},
+            {text:"arequipa, arequipa"},
+            {text:"caraveli, arequipa"},
+            {text:"condesuyo, arequipa"}
+        ],
+        Kitchen:[
+            {text:"choose your kitchen"},
+            {text:"breakfast place"},
+            {text:"restaurants"},
+            {text:"coffees"},
+            {text:"cakeshop"},
+            {text:"bar and pubs"}
+        ]
 
+    }
+})
 
