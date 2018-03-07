23dadb8862433c39b67a62de8641d85e24e3120a
Carlos Chicata Farfan <carlos.fernando.chicata@gmail.com>
2018-03-07 11:04:54 -0500

    finish the header component with new js in bones - frontend

--- RestaFind/static/js/Logistica.js
+++ RestaFind/static/js/Logistica.js
@@ -8,41 +8,68 @@
 /* data global **/
 
 var spa = {
-    Header:{
+    Idioma:{
         Titulo: "RestaFind",
         Subtitulo:"Buscando un buen lugar por un buen momento",
-        BotonSubmit:"busca"
+        BotonSubmit:"busca",
+        Opciones:[
+            { text:"Elige uno!" },
+            { text:"Desayunos" },
+            { text:"Restaurantes" },
+            { text:"Cafes" },
+            { text:"Pastelerias" },
+            { text:"bar y pubs" }
+        ]
     }
 };
 
 var eng = {
-    Header:{
+    Idioma:{
         Titulo: "RestaFind",
         Subtitulo:"find a good place by a good moment",
-        BotonSubmit : "search"
+        BotonSubmit : "search",
+        Opciones:[
+            { text:"Choose one!" },
+            { text:"Breakfast place" },
+            { text:"Restaurants" },
+            { text:"Coffees" },
+            { text:"Cakeshop" },
+            { text:"Bar and pubs" }
+        ]
     }
 };
+
+var Lugares = [
+    { text:"camana, arequipa" },
+    { text:"ocoña, arequipa" },
+    { text:"arequipa, arequipa" },
+    { text:"caraveli, arequipa" },
+    { text:"condesuyo, arequipa" }
+]
+
+
 
 var lenguaje = [ spa, eng ];
 /****  componentes ****/
 
 var cabeza = {
-    props: ["Text"],
+    props: ["Idioma"],
     data: function(){
         return{
-            Identificador: 1,
-            Texto : Text
+            Ubicacion: Lugares,
+            Identificador: 1
         }
     },
     template : "#cabeza-template",
     methods:{
-            CambioLenguaje: function( idx ){
-                var hijos = this.$el.getElementsByClassName("st-BotonHeader")
-                hijos[ this.Identificador ].classList.remove("st-BotonDecoretionHeader")
-                hijos[ idx ].classList.add("st-BotonDecoretionHeader")
-                this.Identificador = idx
-                console.log( this.Texto[ idx ].Header.Subtitulo )
+        CambioLenguaje: function( idx ){
+            var hijos = this.$el.getElementsByClassName("st-BotonHeader")
+            hijos[ this.Identificador ].classList.remove("st-BotonDecoretionHeader")
+            hijos[ idx ].classList.add("st-BotonDecoretionHeader")
+            this.Identificador = idx
+            this.$emit("cambioidioma", { indice : idx})
         }
+
     }
 };
 
@@ -51,10 +78,16 @@ var cabeza = {
 var app = new Vue({
    el : "#GestorGeneral",
    data:{
-        Idioma: spa,
+        Idiomag : lenguaje[ 1 ]
+   },
+   methods:{
+        CambiarIdioma: function( obj ){
+            this.Idiomag = lenguaje[ obj.indice ]
+        }
    },
    components:{
        "cabezaprincipal" : cabeza
    }
+
 })
 
